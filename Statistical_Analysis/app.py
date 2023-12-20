fb77fd64dd33ff7d3a47beacac995d32395aaf57
Nitya Singh <76156577+NityaSG@users.noreply.github.com>
2023-12-20 21:17:32 +0530

    statistics

--- Statistical_Analysis/app.py
+++ Statistical_Analysis/app.py
@@ -6,35 +6,40 @@ o_column_names=['Drug','Event','a','b','c','d','ROR','Upper Bound',"Lower Bound"
 
 def process(df,target,treshold):
     result=pd.DataFrame(columns=o_column_names)
-    filtered_df = df[df['Product Name'] == target]
-    b=int(filtered_df['Case Num'].count())
-    symptom_counts = filtered_df['Event Preferred Term'].value_counts()
-    symptom_dict = symptom_counts.to_dict()
+    drug_counts = df['Product Name'].value_counts()
+    drug_dict = drug_counts.to_dict()
     min_count = treshold  
-    filtered_dict = {k: v for k, v in symptom_dict.items() if v >= min_count}
-    for event_term in filtered_dict:
-        a=filtered_dict[event_term]
-        event_term_count = df['Event Preferred Term'].value_counts()[event_term]
-        c=event_term_count-a
-        d_filtered_df = df[(df['Product Name'] != target) & (df['Event Preferred Term'] != event_term)]
-        d=count = len(d_filtered_df)
-        ROR=0
-        lower_bound=math.nan
-        upper_bound=math.nan
-        if(c==0 or b==0):
-            ROR=math.nan
-            print("denominator has a null value")
+    filtered_dict0 = {l: w for l, w in drug_dict.items() if w >= min_count}
+    for drug in filtered_dict0:
+        filtered_df  = df[df['Product Name'] == drug]
+        b=int(filtered_df['Case Num'].count())
+        symptom_counts = filtered_df['Event Preferred Term'].value_counts()
+        symptom_dict = symptom_counts.to_dict()
+        min_count = treshold  
+        filtered_dict = {k: v for k, v in symptom_dict.items() if v >= min_count}
+        for event_term in filtered_dict:
+            a=filtered_dict[event_term]
+            event_term_count = df['Event Preferred Term'].value_counts()[event_term]
+            c=event_term_count-a
+            d_filtered_df = df[(df['Product Name'] != target) & (df['Event Preferred Term'] != event_term)]
+            d=count = len(d_filtered_df)
+            ROR=0
+            lower_bound=math.nan
+            upper_bound=math.nan
+            if(c==0 or b==0):
+                ROR=math.nan
+                print("denominator has a null value")
+                
+            else:
+                ROR = (a*d) / (b*c)  # replace with your ROR calculation if different
+                lower_bound = math.exp(math.log(ROR) - (1.96 * math.sqrt(1/a + 1/b + 1/c + 1/d)))
+                upper_bound = math.exp(math.log(ROR) + (1.96 * math.sqrt(1/a + 1/b + 1/c + 1/d)))
             
-        else:
-            ROR = (a*d) / (b*c)  # replace with your ROR calculation if different
-            lower_bound = math.exp(math.log(ROR) - (1.96 * math.sqrt(1/a + 1/b + 1/c + 1/d)))
-            upper_bound = math.exp(math.log(ROR) + (1.96 * math.sqrt(1/a + 1/b + 1/c + 1/d)))
-        
 
-        print(f"95% CI Lower Bound: {lower_bound}")
-        print(f"95% CI Upper Bound: {upper_bound}")
-        new_row = {'Drug':target,'Event':event_term,'a':a,'b':b,'c':c,'d':d,'ROR':ROR,'Upper Bound':upper_bound,'Lower Bound':lower_bound}
-        result=pd.concat(result,pd.DataFrame([new_row]),ignore_index=True)
+            print(f"95% CI Lower Bound: {lower_bound}")
+            print(f"95% CI Upper Bound: {upper_bound}")
+            new_row = {'Drug':drug,'Event':event_term,'a':a,'b':b,'c':c,'d':d,'ROR':ROR,'Upper Bound':upper_bound,'Lower Bound':lower_bound}
+            result=pd.concat([result,pd.DataFrame([new_row])],ignore_index=True)
     return result
 
 
@@ -57,6 +62,7 @@ def main():
             if st.button('Download'):
                 dfr.to_csv('output.csv',index=False)
                 st.success('File saved!')
+            
             
     
     
